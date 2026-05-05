1a15025c6ab36d236c759bfab6f2cc493260f5da
Pokesquirrel <39865970+Pokesquirrel@users.noreply.github.com>
2026-05-05 00:28:08 -0700

    Create LockpickServer.lua

---
+++ ServerScriptService/Game/LockpickServer.lua
@@ -0,0 +1,11 @@
+local event = game.ReplicatedStorage.GameEvents.LockpickEvent
+local Threat = require(game.ServerScriptService.AI.Systems.ThreatSystem)
+local Objective = require(script.Parent.ObjectiveSystem)
+
+event.OnServerEvent:Connect(function(player, success)
+	if success then
+		Objective:Collect(player)
+	else
+		Threat:AddStress(player, 10)
+	end
+end)
