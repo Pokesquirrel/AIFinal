193d7f3552b5a02a710757c6f31d270544238792
Pokesquirrel <39865970+Pokesquirrel@users.noreply.github.com>
2026-05-05 00:28:25 -0700

    Create NoiseService.lua

---
+++ ServerScriptService/Game/NoiseService.lua
@@ -0,0 +1,21 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+
+local NoiseEvent = ReplicatedStorage.GameEvents:WaitForChild("NoiseEvent")
+
+local Sensory = require(game.ReplicatedStorage.Shared.SensoryModule)
+
+NoiseEvent.OnServerEvent:Connect(function(player, position, intensity)
+
+	for _, npc in ipairs(workspace:GetChildren()) do
+		if npc:FindFirstChild("HumanoidRootPart") then
+			local aiRoot = npc.HumanoidRootPart
+
+			if Sensory.CanHear(aiRoot.Position, position, intensity) then
+				npc:SetAttribute("LastHeardPosition", position)
+				npc:SetAttribute("Alerted", true)
+			end
+		end
+	end
+
+end)
