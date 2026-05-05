bd4f43cd96e6f40a68f4a154bd780b657b42c02e
Pokesquirrel <39865970+Pokesquirrel@users.noreply.github.com>
2026-05-05 00:29:05 -0700

    Create AISyncService.lua

---
+++ ServerScriptService/Game/AISyncService.lua
@@ -0,0 +1,23 @@
+local Director = require(game.ServerScriptService.DirectorAI)
+
+local Sync = {}
+
+function Sync:Run()
+	task.spawn(function()
+		while true do
+			local job = Director:PopJob()
+
+			if job then
+				for _, npc in ipairs(workspace:GetChildren()) do
+					if npc:FindFirstChild("Humanoid") then
+						npc:SetAttribute("AI_STATE", job.Type)
+					end
+				end
+			end
+
+			task.wait(0.5)
+		end
+	end)
+end
+
+return Sync
