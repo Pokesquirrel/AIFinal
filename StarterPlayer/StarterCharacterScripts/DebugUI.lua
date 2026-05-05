cb7979a64573b2d56fe3f8cee3bc7d6bb7002561
Pokesquirrel <39865970+Pokesquirrel@users.noreply.github.com>
2026-05-05 00:38:51 -0700

    Create DebugUI.lua

---
+++ StarterPlayer/StarterCharacterScripts/DebugUI.lua
@@ -0,0 +1,13 @@
+local event = game.ReplicatedStorage.GameEvents.DebugMetrics
+
+local gui = Instance.new("ScreenGui", game.Players.LocalPlayer.PlayerGui)
+
+local label = Instance.new("TextLabel")
+label.Size = UDim2.fromScale(0.3, 0.2)
+label.Position = UDim2.fromScale(0.7, 0.1)
+label.TextScaled = true
+label.Parent = gui
+
+event.OnClientEvent:Connect(function(data)
+	label.Text = "Detections: "..data.DetectionCount
+end)
