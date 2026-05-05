8a5bea6097419288e14d58553ba557587f183d3e
Pokesquirrel <39865970+Pokesquirrel@users.noreply.github.com>
2026-05-05 00:29:47 -0700

    Create GameStateManager.lua

---
+++ ServerScriptService/Game/GameStateManager.lua
@@ -0,0 +1,20 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local event = ReplicatedStorage.GameEvents.GameStateEvent
+
+local GameState = {}
+
+function GameState:Win(player)
+	event:FireAllClients("WIN", player.Name)
+end
+
+function GameState:Lose(player)
+	event:FireAllClients("LOSE", player.Name)
+end
+
+function GameState:Restart()
+	for _, p in ipairs(game.Players:GetPlayers()) do
+		p:LoadCharacter()
+	end
+end
+
+return GameState
