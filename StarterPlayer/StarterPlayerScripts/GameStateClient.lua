d54d631263dc0e980dcf5ca78a981bc785ab5d14
Pokesquirrel <39865970+Pokesquirrel@users.noreply.github.com>
2026-05-06 00:33:20 -0700

    Update GameStateClient.lua

--- StarterPlayer/StarterPlayerScripts/GameStateClient.lua
+++ StarterPlayer/StarterPlayerScripts/GameStateClient.lua
@@ -1,12 +1,11 @@
+--This is used mainly to for the victory screen popup and keep the game moving when the server starts
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local GameStateEvent = ReplicatedStorage:WaitForChild("GameEvents"):WaitForChild("GameStateEvent")
 
 GameStateEvent.OnClientEvent:Connect(function(state, playerName)
 	if state == "WIN" then
 		print(playerName .. " won!")
-		-- Add win UI/logic here
 	elseif state == "LOSE" then
 		print(playerName .. " lost!")
-		-- Add lose UI/logic here
 	end
 end)
